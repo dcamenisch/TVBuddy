26f7746855583a126f7bf58e6b7ed1c6da694a39
dcamenisch <danny@camenisch.ws>
2023-11-10 14:40:43 +0100

    added convenience init based on TMDb models

--- TVBuddy/Models/TVEpisode.swift
+++ TVBuddy/Models/TVEpisode.swift
@@ -13,20 +13,27 @@ import TMDb
 final class TVEpisode {
     @Attribute(.unique)
     let id: Int
-    
-    var tvSeries: TVSeries?
-    
+
+    var tvShow: TVShow?
+
     let episodeNumber: Int
     let seasonNumber: Int
-        
+
     var watched: Bool
-    
-    init(tvEpisode: TMDb.TVShowEpisode, watched: Bool = false) {
-        self.id = tvEpisode.id
-                
-        self.episodeNumber = tvEpisode.episodeNumber
-        self.seasonNumber = tvEpisode.seasonNumber
-        
+
+    init(id: Int, episodeNumber: Int, seasonNumber: Int, watched: Bool) {
+        self.id = id
+        self.episodeNumber = episodeNumber
+        self.seasonNumber = seasonNumber
         self.watched = watched
+    }
+
+    convenience init(episode: TMDb.TVShowEpisode, watched: Bool = false) {
+        self.init(
+            id: episode.id,
+            episodeNumber: episode.episodeNumber,
+            seasonNumber: episode.seasonNumber,
+            watched: watched
+        )
     }
 }
---
+++ TVBuddy/Models/TVShow.swift
@@ -0,0 +1,41 @@
+//
+//  TVShow.swift
+//  TVBuddy
+//
+//  Created by Danny on 24.09.2023.
+//
+
+import Foundation
+import SwiftData
+import TMDb
+
+@Model
+final class TVShow {
+    @Attribute(.unique)
+    let id: Int
+    let name: String
+
+    @Relationship(deleteRule: .cascade, inverse: \TVEpisode.tvShow)
+    var episodes: [TVEpisode] = []
+
+    var startedWatching: Bool
+    var finishedWatching: Bool
+
+    init(id: Int, name: String, startedWatching: Bool, finishedWatching: Bool) {
+        self.id = id
+        self.name = name
+        self.startedWatching = startedWatching
+        self.finishedWatching = finishedWatching
+    }
+
+    convenience init(
+        tvShow: TMDb.TVShow, startedWatching: Bool = false, finishedWatching: Bool = false
+    ) {
+        self.init(
+            id: tvShow.id,
+            name: tvShow.name,
+            startedWatching: startedWatching,
+            finishedWatching: finishedWatching
+        )
+    }
+}
