85c76d843288ab6fc5b9dd2dcbc6c69a9495532a
dcamenisch <danny@camenisch.ws>
2024-01-05 13:14:54 +0100

    Add watched tv shows and movies to profil

--- TVBuddy/Views/ProfilView.swift
+++ TVBuddy/Views/ProfilView.swift
@@ -10,15 +10,53 @@ import SwiftUI
 import TMDb
 
 struct ProfilView: View {
+    @Query(filter: #Predicate<TVBuddyTVShow> { $0.isArchived })
+    private var archivedTVShows: [TVBuddyTVShow]
+    
+    @Query(filter: #Predicate<TVBuddyTVShow> { $0.finishedWatching })
+    private var watchedTVShows: [TVBuddyTVShow]
+    
     @Query(filter: #Predicate<TVBuddyMovie> { $0.watched })
     private var watchedMovies: [TVBuddyMovie]
 
-    @Query(filter: #Predicate<TVBuddyTVShow> { $0.finishedWatching })
-    private var watchedTVShows: [TVBuddyTVShow]
-
     var body: some View {
+        let _ = Self._printChanges()
+        
         ScrollView(.vertical, showsIndicators: false) {
             VStack(alignment: .leading, spacing: 10) {
+                HStack {
+                    VStack(alignment: .center, spacing: 5) {
+                        Text("\(watchedMovies.count)")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        Text("Watched Movies")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                        Spacer(minLength: 0)
+                    }
+                    .padding(.vertical)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.background2)
+                    .cornerRadius(10)
+
+                    VStack(alignment: .center, spacing: 5) {
+                        Text("\(watchedTVShows.count)")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        Text("Watched TV Shows")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                        Spacer(minLength: 0)
+                    }
+                    .padding(.vertical)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.background2)
+                    .cornerRadius(10)
+                }
+                
+                MediaCollection(title: "Archived TV Shows (\(archivedTVShows.count))", tvShows: archivedTVShows)
                 MediaCollection(title: "Watched TV Shows (\(watchedTVShows.count))", tvShows: watchedTVShows)
                 MediaCollection(title: "Watched Movies (\(watchedMovies.count))", movies: watchedMovies)
             }
