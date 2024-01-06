22ff34ff61d51a9ea8a67788a2328be1f6fe4562
dcamenisch <danny@camenisch.ws>
2024-01-06 14:40:07 +0100

    Add logging to SearchManager.swift

--- TVBuddy/Managers/SearchManager.swift
+++ TVBuddy/Managers/SearchManager.swift
@@ -5,17 +5,31 @@
 //  Created by Danny on 02.07.22.
 //
 
+import os
 import Foundation
 import TMDb
 
 class SearchManager {
+    private static let logger = Logger(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: String(describing: SearchManager.self)
+    )
+    
     let searchService = SearchService()
 
     func search(query: String, page: Int = 1) async -> [Media]? {
         do {
             return try await searchService.searchAll(query: query, page: page).results
+        } catch TMDbError.network(let error) {
+            if let error = error as NSError?, error.code == NSURLErrorCancelled {
+                SearchManager.logger.info("Search request cancelled")
+                return nil
+            }
+            
+            SearchManager.logger.error("\(error.localizedDescription)")
+            return nil
         } catch {
-            print("Error: \(error)")
+            SearchManager.logger.error("\(error.localizedDescription)")
             return nil
         }
     }
